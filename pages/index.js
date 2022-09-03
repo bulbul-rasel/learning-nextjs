c8adc5fdfdc3eae093dfb6e5563dcab7480317f8
bulbul-rasel <rasel.bulbul7@gmail.com>
2022-09-03 12:32:31 +0600

    add 404 page custome

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,12 @@
 import Link from "next/link"
+import { useRouter } from 'next/router';
 export default function Home() {
+  const router = useRouter();
+
+  const handleClick = ()=>{
+    console.log('Successfully Placing order');
+    router.push('/product');
+  }
   return (
     <div>
 
@@ -11,6 +18,8 @@ export default function Home() {
     <Link href='/product'>
     <a>Products</a>
     </Link>
+
+    <button onClick= {handleClick}> Place Order</button>
     </div>
   )
 }
