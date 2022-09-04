95b826b3c8fb544fa30fcd12bc4bf213440534e6
bulbul-rasel <rasel.bulbul7@gmail.com>
2022-09-04 09:51:10 +0600

    get static path bloking method

--- pages/posts/[postId].js
+++ pages/posts/[postId].js
@@ -1,6 +1,12 @@
 import React from 'react';
+import { useRouter } from 'next/router';
 
 const Post = ({post}) => {
+    const router = useRouter()
+
+    // if(router.isFallback){
+    //     return <h1> Loading....</h1>
+    // }
     return (
         <>
          <h2>{post.id}</h2>   
@@ -12,6 +18,17 @@ const Post = ({post}) => {
 export default Post;
 
 export async function getStaticPaths(){
+    // const res = await fetch('https://jsonplaceholder.typicode.com/posts')
+    // const data = await res.json();
+
+    // const paths = data.map(post=>{
+    //     return{
+    //         params:{
+    //             postId : `${post.id}`,
+    //         },
+    //     }
+    // })
+
     return{
         paths:[
             {
@@ -24,7 +41,8 @@ export async function getStaticPaths(){
                 params:{postId:'3'}
             },
         ],
-        fallback:false,
+        // paths,
+        fallback:'blocking',
     }
 }
 
@@ -33,8 +51,16 @@ export async function getStaticProps(context){
     const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${params.postId}`)
     const data = await res.json();
 
+    if(!data.id){
+        return{
+            notFound: true,
+        }
+    }
+    console.log( `Generating page for /posts/${params.postId}`);
+
     return{
         props:{
+            
             post:data,
         },
     }
